0675636f93a03d217cfeb0e56a861fb50fa23362
Ternyx <tixtix293@gmail.com>
2020-04-06 01:09:12 +0300

    Add Seat test

---
+++ src/test/java/com/github/ternyx/models/SeatTest.java
@@ -0,0 +1,21 @@
+package com.github.ternyx.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+/**
+ * SeatTest
+ */
+public class SeatTest {
+    
+    @Test
+    public void test_Seat_NegativeSeat_ExceptionThrown() {
+        assertThrows(IllegalArgumentException.class, () -> new Seat('a', (short) -1));
+    }
+
+    @Test
+    public void test_Seat_NonNegativeSeat_ReturnsInput() {
+        assertEquals(0, new Seat('a', (short) 0).getSeat());
+    }
+}
