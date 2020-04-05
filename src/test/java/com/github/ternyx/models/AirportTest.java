146c90814230fa35f2c5eb3c1986b577f71cf280
Ternyx <tixtix293@gmail.com>
2020-04-06 01:24:58 +0300

    Add airport test

---
+++ src/test/java/com/github/ternyx/models/AirportTest.java
@@ -0,0 +1,28 @@
+package com.github.ternyx.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.github.ternyx.enums.AirportName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * AirportTest
+ */
+public class AirportTest {
+
+    @Test
+    public void test_Airport_NullParameters_ExceptionThrown() {
+        assertThrows(NullPointerException.class, () -> new Airport(null, 0));
+    }
+
+    @Test
+    public void test_Airport_NegativeCapacity_ExceptionThrown() {
+        assertThrows(IllegalArgumentException.class, () -> new Airport(AirportName.RIGA, -1));
+    }
+
+    @Test 
+    public void test_Airport_ValidCapacity_ReturnsInput() {
+        assertEquals(100, new Airport(AirportName.RIGA, 100));
+    }
+}
